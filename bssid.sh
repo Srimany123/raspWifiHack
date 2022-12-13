8990c9301a79d8e837f00546482c1c1a635220d2
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-12-13 21:25:28 +0530

    Update bssid.sh

--- bssid.sh
+++ bssid.sh
@@ -1,3 +1,4 @@
 read -p "Enter the bssid of the hacking wifi-network: " bssid
 read -p "Enter the channel: " channel
-sudo airodump-ng wlan0 -d $bssid --channel $channel
+read -p "Enter your wifi-adapter name : " Adap
+sudo airodump-ng $Adap -d $bssid --channel $channel
